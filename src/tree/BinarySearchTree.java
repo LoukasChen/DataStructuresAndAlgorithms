8e7e5190fcf7cc222cc7fe750a49a84c251cf5ec
csp <jeffcsp@163.com>
2019-07-14 22:48:26 +0800

    二叉搜索树

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -7,8 +7,6 @@ package tree;
  */
 public class BinarySearchTree {
 
-    private Node node;
-
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         Node root = new Node(7);
@@ -18,6 +16,10 @@ public class BinarySearchTree {
         root.left.right = new Node(5);
         root.right.right = new Node(12);
         System.out.println(binarySearchTree.find(root, 5));
+        binarySearchTree.insert(root, 2);
+        System.out.println("---------------");
+        binarySearchTree.inOrder(root);
+        System.out.println();
     }
 
     public Node find(Node node, int data) {
@@ -35,6 +37,50 @@ public class BinarySearchTree {
         return null;
     }
 
+    public void insert(Node node, int data) {
+        Node newNode = new Node(data);
+        if (node == null) {
+            return;
+        }
+        while (node != null) {
+            if (data > node.data) {
+                if (node.right == null) {
+                    node.right = newNode;
+                    return;
+                } else {
+                    node = node.right;
+                }
+            } else {
+                if (node.left == null) {
+                    node.left = newNode;
+                    return;
+                } else {
+                    node = node.left;
+                }
+            }
+        }
+    }
+
+    public void delete(Node node, int data) {
+        if (node == null) {
+            return;
+        } else {
+            while (node != null) {
+                if ((node.left.data == data)) {
+                    node.left = null;
+                } else if (node.right.data == data) {
+                    node.right = null;
+                } else {
+                    return;
+                }
+            }
+        }
+    }
+
+    public void inOrder(Node node) {
+        node.inOrder(node);
+    }
+
     static class Node {
         private int data;
         private Node left;
@@ -49,6 +95,21 @@ public class BinarySearchTree {
             return "Node{" +
                     "data=" + data +
                     '}';
+        }
+
+        /**
+         * 中序遍历打印二叉排序树
+         *
+         * @param node
+         */
+        public void inOrder(Node node) {
+            if (node.left != null) {
+                inOrder(node.left);
+            }
+            System.out.println(node);
+            if (node.right != null) {
+                inOrder(node.right);
+            }
         }
     }
 
